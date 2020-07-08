01904a527c9a4ca2b9053a2a95bdc477febe6efb
Robertus B <obetckx@gmail.com>
2020-07-08 20:52:05 +0700

    add log

--- app/src/main/java/com/obet/rolldice/presenter/DicePresenter.kt
+++ app/src/main/java/com/obet/rolldice/presenter/DicePresenter.kt
@@ -13,10 +13,34 @@ class DicePresenter {
 
     fun diceRoll() {
         when ((1..6).random()) {
-            1, 3, 5 -> score += 5
-            2, 4, 6 -> score -= 3
+//            1, 3, 5 -> score += 5
+//            2, 4, 6 -> score -= 3
+            1 -> {
+                score += 5
+                Log.d("dadu", "1")
+            }
+            2 -> {
+                score -= 3
+                Log.d("dadu", "2")
+            }
+            3 -> {
+                score += 5
+                Log.d("dadu", "3")
+            }
+            4 -> {
+                score -= 3
+                Log.d("dadu", "4")
+            }
+            5 -> {
+                score += 5
+                Log.d("dadu", "5")
+            }
+            6 -> {
+                score -= 3
+                Log.d("dadu", "6")
+            }
         }
         view.result("$score")
-        Log.d("Score", score.toString() )
+        Log.d("Score", score.toString())
     }
 }
